e6e8ab80452193e1ece430d2901e8cddae9188fd
MasatoArima <afoc.0707@gmail.com>
2021-12-11 11:17:27 +0900

    main

--- MCS.py
+++ MCS.py
@@ -1,4 +1,3 @@
-from old_mcs import Asum_Leaf
 import pydicom
 import rtf
 import numpy as np
@@ -6,8 +5,10 @@ import numpy as np
 df = pydicom.read_file("RTplan.dcm")  # 取得したいRTplanのファイル名をRTplanにする
 
 beam_number = df.FractionGroupSequence[0].NumberOfBeams  # Arc数取得
-Jaw_ref = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
-Jaw_ref2 = [0, -5, -10, -15, -20, -25, -30, -35, -40, -45, -50, -55, -60, -65, -70, -75, -80, -85, -90, -95, -100, -110, -120, -130, -140, -150, -160, -170, -180, -190, -200]
+Jaw_ref = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75,
+           80, 85, 90, 95, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
+Jaw_ref2 = [0, -5, -10, -15, -20, -25, -30, -35, -40, -45, -50, -55, -60, -65, -70, -
+            75, -80, -85, -90, -95, -100, -110, -120, -130, -140, -150, -160, -170, -180, -190, -200]
 
 cp_list = []
 for i in range(beam_number):
@@ -38,8 +39,7 @@ for bi in range(beam_number):
 
 y1jaw = rtf.beamnumber_split(y1jaw)
 
-
-tmp =[]
+tmp = []
 y1_a_bank = []
 y1_b_bank = []
 y2_a_bank = []
@@ -136,8 +136,6 @@ for i in range(cp_sum):
 mlc_a_bank.append(tmp)
 mlc_a_bank.pop(0)
 
-mlc_a_bank = rtf.beamnumber_split(mlc_a_bank)
-mlc_b_bank = rtf.beamnumber_split(mlc_b_bank)
 
 # Abank & Bbank特徴値取得
 Asum_Leaf = []
@@ -153,10 +151,48 @@ for i in range(cp_sum):
     Amax_Leaf.append(max((Asum_Leaf[i])))
     Amin_Leaf.append(min((Asum_Leaf[i])))
     Asub_Leaf.append((Amax_Leaf[i])-(Amin_Leaf[i]))
-    Bsum_Leaf.append((y1_b_bank[i])+(Ly2_b_bank[i]))
+    Bsum_Leaf.append((y1_b_bank[i])+(y2_b_bank[i]))
     Bmax_Leaf.append(max((Bsum_Leaf[i])))
     Bmin_Leaf.append(min((Bsum_Leaf[i])))
     Bsub_Leaf.append((Bmax_Leaf[i])-(Bmin_Leaf[i]))
 
-print(len(mlc_a_bank))
-print(len(mlc_b_bank))
+sum_list_A = []
+sum_list_B = []
+
+for j in range(cp_sum):
+    list_A = []
+    list_B = []
+    sum_list_A.append(list_A)
+    sum_list_B.append(list_B)
+    for r in range(len(mlc_a_bank[j])):
+        list_A.append(Asub_Leaf[j]-mlc_a_bank[j][r])
+    for r in range(len(mlc_b_bank[j])):
+        list_B.append(Bsub_Leaf[j]-mlc_b_bank[j][r])
+
+# SUM
+sum_A = []
+sum_B = []
+count_A = []
+count_B = []
+
+for j in range(cp_sum):
+    sum_A.append(sum(sum_list_A[j]))
+    sum_B.append(sum(sum_list_B[j]))
+    count_A.append(len(mlc_a_bank[j]))
+    count_B.append(len(mlc_b_bank[j]))
+
+sum_max_F1_A = []
+sum_max_F1_B = []
+for j in range(cp_sum):
+    sum_max_F1_A.append(count_A[j]*Asub_Leaf[j])
+    sum_max_F1_B.append(count_B[j]*Bsub_Leaf[j])
+
+LSV_A = []
+LSV_B = []
+for j in range(cp_sum):
+    LSV_A.append(sum_A[j]/sum_max_F1_A[j])
+    LSV_B.append(sum_B[j]/sum_max_F1_B[j])
+
+LSV_CP = []
+for j in range(cp_sum):
+    LSV_CP.append(LSV_A[j]*LSV_B[j])
