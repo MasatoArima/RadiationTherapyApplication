a11893494cff91a5914cac4c1d0833a57f7c4f11
MasatoArima <afoc.0707@gmail.com>
2022-01-14 08:46:45 +0900

    main

--- test.py
+++ test.py
@@ -11,3 +11,37 @@ fig, axes = plt.subplots(2)
 axes[0].plot(a, label='legend label')
 axes[1].plot(b, label='legend label')
 plt.show()
+
+
+
+# stracture読み込み必要
+
+b = a["BODY"]
+
+list_z = list(b.keys())
+
+tmp_a = []
+tmp_b = []
+tmp_c = []
+    
+for r in list_z :
+    for i in range (len(b[r][0])):
+        tmp_a.append(b[r][0][i][0])
+        tmp_b.append(b[r][0][i][1])
+        tmp_c.append(r)
+    
+
+
+from mpl_toolkits.mplot3d import Axes3D
+# Figureを追加
+fig = plt.figure(figsize=(8, 8))
+
+# 3DAxesを追加
+ax = fig.add_subplot(111, projection='3d')
+
+
+# Axesのタイトルを設定
+ax.scatter(tmp_a,tmp_b,tmp_c)
+plt.xlim(-500, 500)
+plt.ylim(-200, 200)
+plt.show()
