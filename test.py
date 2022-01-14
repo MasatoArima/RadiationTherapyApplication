c7c91e7d3feb4bab59b5c67b8e2a09ccb02da6eb
MasatoArima <afoc.0707@gmail.com>
2022-01-14 21:07:30 +0900

    main

--- test.py
+++ test.py
@@ -1,47 +1,50 @@
 import pydicom
 import matplotlib.pyplot as plt
 import matplotlib.style
+from mpl_toolkits.mplot3d import Axes3D
 matplotlib.style.use('ggplot')
 
-df = pydicom.read_file("tes1.dcm")
-
-a = df.ROIContourSequence[1].ContourSequence[0].ContourData
-b = df.ROIContourSequence[1].ContourSequence[1].ContourData
-fig, axes = plt.subplots(2)
-axes[0].plot(a, label='legend label')
-axes[1].plot(b, label='legend label')
-plt.show()
-
-
+df = pydicom.read_file("RTplan.dcm")
 
 # stracture読み込み必要
 
-b = a["BODY"]
+origin = input('DICOMoriginを入力してください : ')
 
-list_z = list(b.keys())
+b = a["Skin"]
+b1 = a["PTV_66"]
 
+list_y = list(b.keys())
+list_y1 = list(b1.keys())
 tmp_a = []
 tmp_b = []
 tmp_c = []
+tmp_a1 = []
+tmp_b1 = []
+tmp_c1 = []
     
-for r in list_z :
+for r in list_y :
     for i in range (len(b[r][0])):
         tmp_a.append(b[r][0][i][0])
         tmp_b.append(b[r][0][i][1])
-        tmp_c.append(r)
-    
+        tmp_c.append(origin + r)
+
+for r in list_y1 :
+    for i in range (len(b1[r][0])):
+        tmp_a1.append(b1[r][0][i][0])
+        tmp_b1.append(b1[r][0][i][1])
+        tmp_c1.append(origin + r)
 
 
-from mpl_toolkits.mplot3d import Axes3D
-# Figureを追加
-fig = plt.figure(figsize=(8, 8))
-
-# 3DAxesを追加
+fig = plt.figure(figsize=(5, 5))
 ax = fig.add_subplot(111, projection='3d')
+ax.plot(tmp_a,tmp_b,tmp_c)
+ax.plot(tmp_a1,tmp_b1,tmp_c1, color='blue')
+ax.view_init(elev=0, azim=270)
+plt.show()
 
-
-# Axesのタイトルを設定
-ax.scatter(tmp_a,tmp_b,tmp_c)
-plt.xlim(-500, 500)
-plt.ylim(-200, 200)
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(tmp_a,tmp_b,tmp_c)
+ax.plot(tmp_a1,tmp_b1,tmp_c1, color='blue')
+ax.view_init(elev=20, azim=200)
 plt.show()
