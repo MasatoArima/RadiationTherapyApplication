863eb21baee63a7e0983a4918c8a68da4710c6a2
MasatoArima <afoc.0707@gmail.com>
2022-01-22 22:40:28 +0900

    main

---
+++ neural_network.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+
+# 目的変数の加工処理で必要なライブラリ
+from keras.utils.np_utils import to_categorical
+
+# warningを表示させない
+warnings.simplefilter('ignore')
