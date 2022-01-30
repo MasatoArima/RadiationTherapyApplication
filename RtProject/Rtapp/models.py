f2450687b5a43c647c782294d176db930f13ae01
MasatoArima <afoc.0707@gmail.com>
2022-01-30 11:14:43 +0900

    main

--- RtProject/Rtapp/models.py
+++ RtProject/Rtapp/models.py
@@ -1,3 +1,33 @@
+import email
+from statistics import mode
+from venv import create
 from django.db import models
+from django.utils import timezone
+import pytz
 
 # Create your models here.
+
+class BaseMeta(models.Model):
+    create_at = models.DateTimeField(default=timezone.datetime.now(pytz.timezone('Asia/Tokyo')))
+    update_at = models.DateTimeField(default=timezone.datetime.now(pytz.timezone('Asia/Tokyo')))
+    
+    class Meta:
+        abstract = True #Tableとして作成されないようにする
+
+
+class Person(BaseMeta): # BaseMetaを継承させている
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthday = models.DateField(default='1900-01-01')
+    email = models.EmailField(db_index=True)
+    salary = models.FloatField(null=True)
+    memo = models.TextField()
+    web_site = models.URLField(null=True, blank=True)
+    
+    class Meta:
+        db_table = 'Person'
+        index_together = [['first_name','last_name']]
+        ordering = ['salary']
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
