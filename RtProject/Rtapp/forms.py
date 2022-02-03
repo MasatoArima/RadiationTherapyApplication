3683b50ed135789da1801ea140b54b34e315d0ed
MasatoArima <afoc.0707@gmail.com>
2022-02-04 07:07:00 +0900

    main

--- RtProject/Rtapp/forms.py
+++ RtProject/Rtapp/forms.py
@@ -1,11 +1,19 @@
 from socket import fromshare
+from bleach import clean
 from click import Choice
 from django import forms
+from django.core import validators
+
+def check_name(value):
+    if value == 'あああああ':
+        raise forms.ValidationError('適応できません')
+    
 
 class UserInfo(forms.Form):
-    name = forms.CharField(label='名前', min_length=5, max_length=10)
-    age = forms.IntegerField(label='年齢')
+    name = forms.CharField(label='名前', min_length=5, max_length=10, validators=[check_name])
+    age = forms.IntegerField(label='年齢', validators=[validators.MinValueValidator(20, message='20才以上のみ')])
     mail = forms.EmailField(label='メールアドレス', widget=forms.TextInput(attrs={'class': 'mail-class', 'placeholder': 'sample@mail.com'}))
+    verify_mail = forms.EmailField(label='再メールアドレス', widget=forms.TextInput(attrs={'class': 'mail-class', 'placeholder': 'sample@mail.com'}))
     is_married = forms.BooleanField(initial=True)
     birthday = forms.DateField(initial='1990-01-01')
     salary = forms.DecimalField()
@@ -28,3 +36,15 @@ class UserInfo(forms.Form):
         super(UserInfo, self).__init__(*args, **kwargs)
         self.fields['job'].widget.attrs['id'] = 'id_job'
         self.fields['hobbies'].widget.attrs['class'] = 'hobbies_class'
+    
+    def clean_homepage(self):
+        homepage = self.cleaned_data['homepage']
+        if not homepage.startswith('https'):
+            raise forms.ValidationError('ホームページのURLはhttpsのみです！')
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        mail = cleaned_data['mail']
+        verify_mail = cleaned_data['verify_mail']
+        if mail != verify_mail:
+            raise forms.ValidationError('一致しません！')
